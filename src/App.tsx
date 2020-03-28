f64dda0911a1407c77fde4bdf0a44ff29ca6adeb
Ed Timmer <edtimmer@gmail.com>
2020-03-28 14:01:56 -0400

    can count press time and pause time

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+
+  const [isDot, setIsDot] = useState<boolean>(true);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
+
+  useEffect(() => {
+    let pressed: boolean = false;
+    let startTime: number = 0;
+    let endTime: number = 0;
+    let elapsedTime: number = 0;
+
+    let pauseTimer: any;
+
+    const pauseFunc = () => {
+      pauseTimer = setTimeout(() => setIsPaused(true), 1000);
+    }
+
+    function stopTimer() {
+      clearTimeout(pauseTimer);
+    }
+
+    const pressKey = (event: any) => {
+      if (event.keyCode === 32 && !pressed) {
+        pressed = true;
+        startTime = Date.now();
+        setIsPaused(false);
+        stopTimer();
+
+      }
+    };
+
+    const liftKey = (event: any) => {
+      if (event.keyCode === 32) {
+        endTime = Date.now();
+
+        if (endTime !== 0) {
+          elapsedTime = endTime - startTime;
+          pressed = false;
+          elapsedTime < 150 ? setIsDot(true) : setIsDot(false);
+        }
+        
+        pauseFunc()
+      }
+    };
+    // initiate the event handler
+    // if (!pressed) {
+    window.addEventListener("keydown", pressKey, false);
+    // }
+
+    // if (pressed) {
+    window.addEventListener("keyup", liftKey, false);
+    // }
+
+    // this will clean up the event every time the component is re-rendered
+    return function cleanup() {
+      window.removeEventListener("keydown", pressKey);
+      window.removeEventListener("keyup", liftKey);
+    };
+  }, [isPaused]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      {isDot ? <div>.</div> : <div>--</div>}
+      {isPaused ? <div>Pause</div> : null}
+    
+    </div>);
 }
 
 export default App;
